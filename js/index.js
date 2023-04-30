ff8fb8dd8703ff21d177a01dc8a96d0dbf42823c
Camila Buldin <cami@Camilas-MacBook-Air.local>
2023-05-01 00:41:57 +0200

    me estresé

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+
+
+
+const roadPic = document.createElement('img');
+roadPic.src = './images/road.png';
+
+const carPic = document.createElement('img');
+carPic.src = './images/car.png'
+
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
@@ -5,3 +17,13 @@ window.onload = () => {
 
   function startGame() {}
 };
+
+roadPic.onload = () => {
+  ctx.drawImage(roadPic, 0, 0, 500, 700);
+};
+
+// roadPic.onload = () => {
+//   ctx.drawImage(carPic, 0, 0, 50, 70);
+// };
+
+
