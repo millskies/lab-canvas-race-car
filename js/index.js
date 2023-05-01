08de170cf61b46b317ad9767d5d5825c98fc2440
Camila Buldin <cami@Camilas-MacBook-Air.local>
2023-05-01 09:11:57 +0200

    logre poner el carrito e intente hacer d e i:)

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext('2d');
 
-
-
 const roadPic = document.createElement('img');
 roadPic.src = './images/road.png';
 
@@ -22,8 +20,28 @@ roadPic.onload = () => {
   ctx.drawImage(roadPic, 0, 0, 500, 700);
 };
 
-// roadPic.onload = () => {
-//   ctx.drawImage(carPic, 0, 0, 50, 70);
-// };
+carPic.onload = () => {
+  ctx.drawImage(carPic, 222, 600, 50, 70);
+};
+
+
+document.addEventListener("keydown", (event) => {
+  switch(true){
+   case (event.key === "ArrowLeft" && carPic.x > 0):
+     console.log("arrowleft")
+    carPic.x -= 30 // 
+     break;
+   case event.key === "ArrowRight" &&carPic.x < 750:
+     console.log("arrowright");
+    carPic.x += 30;
+     break;
+ }
+});
+
+// document.addEventListener("keyup", (event) => {
+//  mypedrito.speedX = 0;
+//  mypedrito.speedY = 0;
+//  // this.y = 0;
+// });
 
 
